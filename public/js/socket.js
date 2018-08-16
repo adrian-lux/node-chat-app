e1d72b6cfc21d6cb8c2cc1cb47dbadf9763a4cf7
adrian-lux <adorian@akita-itlabo.com>
2018-08-16 15:58:28 +0900

    chat server backend ready

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to Server');
-
-    socket.emit('createMessage', {
-        from: 'username',
-        text: 'Dummy Text.'
-    });
 });
 socket.on('disconnect',() => {
     console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        text: 'Hey whats up?',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log("A new Email was Delivered", message);
-    });
+    
+            io.emit('newMessage',{
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()   
+            });
+        });
 
     socket.on('disconnect', () =>{
         console.log('User disconnected');
