b4f266fe2a81d18d0611d830b7b35e1a0c75eae4
adrian-lux <adorian@akita-itlabo.com>
2018-08-16 16:35:41 +0900

    Greet new user

--- server/server.js
+++ server/server.js
@@ -15,14 +15,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the Chat",
+        time: new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage',
+        {from: "Admin",
+        text: "New User Joined",
+        time: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log("A new Email was Delivered", message);
     
-            io.emit('newMessage',{
+       io.emit('newMessage',{
                 from: message.from,
                 text: message.text,
                 createdAt: new Date().getTime()   
-            });
+            }); 
+
+          /*   socket.broadcast.emit('newMessage',{
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
+            });  */   
+
         });
 
     socket.on('disconnect', () =>{
